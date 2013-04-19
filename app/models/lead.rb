344182eddb7181c26ec6c75a1d82d64740c177c1
Aaron King <aaron.w.king@gmail.com>
2013-04-19 00:11:09 -0700

    add map to lead show, geocode on full address

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,6 +1,9 @@
 class Lead < ActiveRecord::Base
   attr_accessible :company_name, :contact, :email, :notes, :phone, :website, :address, :city, :state, :zipcode, :company_type, :inactive, :latitude, :longitude
-  geocoded_by :zipcode
+  def full_address
+    [address, city, state, zipcode].compact.join(', ')
+  end
+  geocoded_by :full_address
   after_validation :geocode, :if => :address_changed?
 
   belongs_to :user
